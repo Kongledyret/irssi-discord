00974d3c1b12a0daed97e4299d5709c2a6734550
Marco Sirabella <marco@sirabella.org>
2017-09-03 12:49:15 -0400

    Use custom structs more and memory safety

--- src/servers.c
+++ src/servers.c
@@ -22,9 +22,9 @@ static DISCORD_SERVER_REC *init(DISCORD_SERVER_CONNECT_REC *connrec) {
 	server->chat_type = PROTOCOL;
 	server->connrec = connrec;
 
-	server->tok = connrec->tok;
+	server->tok = g_strdup(connrec->tok);
 
-	connrec->address = "gateway.discord.gg";
+	connrec->address = g_strdup("gateway.discord.gg");
 	connrec->port = 443; // TODO: make this default a better way
 
 	server_connect_ref(SERVER_CONNECT(connrec));
@@ -32,15 +32,15 @@ static DISCORD_SERVER_REC *init(DISCORD_SERVER_CONNECT_REC *connrec) {
 	return server;
 }
 
-static void sig_connected(SERVER_REC *server) {
+static void sig_connected(DISCORD_SERVER_REC *server) {
 	debug();
 	server->connected = true; // XXX \\ Why do I have to do this tho
 }
 
-static void connect(SERVER_REC *server) {
+static void connect(DISCORD_SERVER_REC *server) {
 	debug();
-	printf("%d", server_start_connect(server));
-	printf("%d", server->connected);
+	server_start_connect((SERVER_REC *) server);
+	printf("token: %s", server->tok);
 	return;
 }
 
@@ -52,7 +52,7 @@ static void destroy(SERVER_CONNECT_REC *conn) {
 void servers_protocol_init(CHAT_PROTOCOL_REC *rec) {
 	rec->create_server_connect = create;
 	rec->server_init_connect = (SERVER_REC *(*)(SERVER_CONNECT_REC *)) init;
-	rec->server_connect = connect;
+	rec->server_connect = (void (*)(SERVER_REC *)) connect;
 	rec->destroy_server_connect = destroy;
 
 	signal_add_first("server connected", (SIGNAL_FUNC) sig_connected);
